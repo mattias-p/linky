19fd5dd7e6323c1fc5a75e6ebb696bc5906ef1a2
Mattias Päivärinta <mattias.paivarinta@iis.se>
2017-10-23 18:14:55 +0200

    Basic command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "Extract links from Markdown files.")]
+struct Opt {
+    /// Needed parameter, the first on the command line.
+    #[structopt(help = "Files and/or directory to walk")]
+    path: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
