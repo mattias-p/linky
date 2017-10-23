b5a0337214e25721cfd5d3512ea1998b666608a0
Mattias Päivärinta <mattias.paivarinta@iis.se>
2017-10-23 18:27:02 +0200

    Open files

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
 
+use std::fs::File;
+use std::io::Read;
+
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "Extract links from Markdown files.")]
 struct Opt {
     /// Needed parameter, the first on the command line.
-    #[structopt(help = "Files and/or directory to walk")]
-    path: Vec<String>,
+    #[structopt(help = "Files to parse")]
+    file: Vec<String>,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+    for ref path in &opt.file {
+        let mut file = File::open(path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        println!("{} {}", path, contents.len());
+    }
 }
